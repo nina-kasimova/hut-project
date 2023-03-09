0c9946c92355a96e20afac727ecf06f0596b43f8
Ethan Barker <ebarker4@sheffield.ac.uk>
2023-03-09 12:42:45 +0000

    Users can now only view electives and admins have all other permissions

--- app/controllers/electives_controller.rb
+++ app/controllers/electives_controller.rb
@@ -1,5 +1,7 @@
 class ElectivesController < ApplicationController
-  before_action :set_elective, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:index]
+
+  before_action :set_elective, only: %i[ show edit update destroy ]
 
   authorize_resource
 
@@ -15,37 +17,58 @@ class ElectivesController < ApplicationController
 
   # GET /electives/new
   def new
-    @elective = Elective.new
+    if current_user.admin?
+      @elective = Elective.new
+    else
+      redirect_to electives_path, alert: "You do not have permission to perform this action."
+    end
   end
 
   # GET /electives/1/edit
   def edit
+    if current_user.admin?
+      # Proceed with the edit action
+    else
+      redirect_to electives_path, alert: "You do not have permission to perform this action."
+    end
   end
 
   # POST /electives
   def create
-    @elective = Elective.new(elective_params)
+    if current_user.admin?
+      @elective = Elective.new(elective_params)
 
-    if @elective.save
-      redirect_to @elective, notice: "Elective was successfully created."
+      if @elective.save
+        redirect_to @elective, notice: "Elective was successfully created."
+      else
+        render :new, status: :unprocessable_entity
+      end
     else
-      render :new, status: :unprocessable_entity
+      redirect_to electives_path, alert: "You do not have permission to perform this action."
     end
   end
 
   # PATCH/PUT /electives/1
   def update
-    if @elective.update(elective_params)
-      redirect_to @elective, notice: "Elective was successfully updated."
+    if current_user.admin?
+      if @elective.update(elective_params)
+        redirect_to @elective, notice: "Elective was successfully updated."
+      else
+        render :edit, status: :unprocessable_entity
+      end
     else
-      render :edit, status: :unprocessable_entity
+      redirect_to electives_path, alert: "You do not have permission to perform this action."
     end
   end
 
   # DELETE /electives/1
   def destroy
-    @elective.destroy
-    redirect_to electives_url, notice: "Elective was successfully destroyed."
+    if current_user.admin?
+      @elective.destroy
+      redirect_to electives_url, notice: "Elective was successfully destroyed."
+    else
+      redirect_to electives_path, alert: "You do not have permission to perform this action."
+    end
   end
 
   private
