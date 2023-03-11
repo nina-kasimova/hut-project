eb64dc27715e26bb6c31b7e90db5751e10a4cf37
Tom <tblewett1@sheffield.ac.uk>
2023-03-11 12:55:02 +0000

    Changed pending message for tests (nothing yet to test)

--- spec/models/elective_spec.rb
+++ spec/models/elective_spec.rb
@@ -16,5 +16,5 @@
 require 'rails_helper'
 
 RSpec.describe Elective, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  pending "# Currently no methods in the electives model to test #{__FILE__}"
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,5 +25,5 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  pending "# Currently no methods in the user model to test #{__FILE__}"
 end
