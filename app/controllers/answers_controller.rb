263955ff3a72dd510e8f6aad5ac08fe3072a7300
Ethan Barker <ebarker4@sheffield.ac.uk>
2023-03-17 12:45:58 +0000

    Can now see answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -3,14 +3,15 @@ class AnswersController < ApplicationController
     before_action :set_answer, only: [:destroy]
   
     def create
-      @answer = @question.answers.new(answer_params)
-  
-      if @answer.save
-        redirect_to @question, notice: 'Answer was successfully created.'
-      else
-        redirect_to @question, alert: 'Error: Answer was not saved.'
-      end
-    end
+        @question = Question.find(params[:question_id])
+        @answer = @question.answers.build(answer_params)
+      
+        if @answer.save
+          redirect_to @question, notice: 'Answer was successfully created.'
+        else
+          render 'questions/show'
+        end
+      end      
   
     private
   
