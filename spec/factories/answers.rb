5c588e3392e65e53ff198c12f5c62c16c5783128
Ethan Barker <ebarker4@sheffield.ac.uk>
2023-03-24 16:02:51 +0000

    Bug fix

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -3,6 +3,7 @@
 # Table name: answers
 #
 #  id          :bigint           not null, primary key
+#  approved    :boolean
 #  body        :text
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
