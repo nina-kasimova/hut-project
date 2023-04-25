190dc56754ac326e129dc259371c5a4e514a792b
jwhogg <jhogg2@sheffielc.ac.uk>
2023-04-25 11:50:17 +0100

    more security tests

--- spec/features/security_spec.rb
+++ spec/features/security_spec.rb
@@ -22,11 +22,77 @@ RSpec.describe 'User Authentication', type: :feature do
 
 	    expect(page).to have_current_path('/search')
 	end
+
+  	specify 'As a guest, I cannot access WP support without logging in' do
+	  	visit '/'
+	  	visit 'wp_support'
+
+	  	expect(page).to have_content("You need to sign in or sign up before continuing.")
+	    expect(page).to have_current_path('/users/sign_in')
+
+	    sign_in_user
+	    visit '/wp_support'
+
+	    expect(page).to have_current_path('/wp_support')
+	end
+
+  	specify 'As a guest, I cannot access finances without logging in' do
+	  	visit '/'
+	  	visit 'finances'
+
+	  	expect(page).to have_content("You need to sign in or sign up before continuing.")
+	    expect(page).to have_current_path('/users/sign_in')
+
+	    sign_in_user
+	    visit '/finances'
+
+	    expect(page).to have_current_path('/finances')
+	end
+
+  	specify 'As a guest, I cannot access FAQ without logging in' do
+	  	visit '/'
+	  	visit 'faq'
+
+	  	expect(page).to have_content("You need to sign in or sign up before continuing.")
+	    expect(page).to have_current_path('/users/sign_in')
+
+	    sign_in_user
+	    visit '/faq'
+
+	    expect(page).to have_current_path('/faq')
+	end
+
+  	specify 'As a guest, I cannot access admin dashboard without logging in' do
+	  	visit '/'
+	  	visit 'admin_dashboard'
+
+	  	expect(page).to have_content("You need to sign in or sign up before continuing.")
+	    expect(page).to have_current_path('/users/sign_in')
+
+	    sign_in_user
+	    visit '/admin_dashboard'
+
+	    expect(page).to have_current_path('/admin_dashboard')
+	end
+
+  	specify 'As a guest, I cannot access settings without logging in' do
+	  	visit '/'
+	  	visit 'users/edit'
+
+	  	expect(page).to have_content("You need to sign in or sign up before continuing.")
+	    expect(page).to have_current_path('/users/sign_in')
+
+	    sign_in_user
+	    visit '/users/edit'
+
+	    expect(page).to have_current_path('/users/edit')
+	end
 end
 
 RSpec.describe 'Vunerabilities', type: :feature do
 
 	let!(:admin) { FactoryBot.create(:user, email: "admin@sheffield.ac.uk" ,password: "password123", admin: true) }
+	let!(:user) { FactoryBot.create(:user, password: "password123") }
 
 	before :each do
     	nil
@@ -48,6 +114,31 @@ RSpec.describe 'Vunerabilities', type: :feature do
 	  	visit 'search'
 	  	click_on 'SEARCH FOR ELECTIVES'
 	  	expect(page).not_to have_content("SQL_INJECTION")
+	end
+
+	specify "As an admin, I cannot perform XSS attacks" do
+		login_as :admin
+		visit 'search'
+		click_on 'New Elective'
+		fill_in 'elective_Title', with: "<h1>Hello</h1>
+                                <script>
+                                  (function() {
+                                    window.location.replace('http://api.rubyonrails.org/classes/ActionView/Helpers/SanitizeHelper.html');
+                                  })();
+                                </script>"
+
+		click_on 'Save'
+		visit elective_path(Elective.last)
+		expect(current_host).to be_in ['http://127.0.0.1', 'http://localhost']
+		within(:css, 'h1') { expect(page).to have_content 'Hello' }		
+	end
+
+	specify "As a user, I cannot make myself admin", js: true do
+		login_as :user
+		visit '/users/edit'
+    	page.execute_script "document.getElementById('edit_user').innerHTML += \"<input value='t' name='user[manager]'>\""
+
+
 	end
 end
 
