332e91420853d6c18a6f22a635b236065fe2cab6
nina <nkasimova1@sheffield.ac.uk>
2023-04-26 16:33:41 +0100

    style admin dashboard

--- app/packs/entrypoints/styles.js
+++ app/packs/entrypoints/styles.js
@@ -3,4 +3,5 @@ require.context('../images', true);
 import 'bootstrap-icons/font/bootstrap-icons';
 import '../styles/twitter_bootstrap';
 import '../styles/layout';
-import '../styles/background';
+import '../styles/home_page';
+import '../styles/admin-dashboard';
